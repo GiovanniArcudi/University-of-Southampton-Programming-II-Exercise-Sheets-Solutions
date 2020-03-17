223872b2309fd4aa721dafa59b513e9c1c536b64
Giovanni Arcudi <56199876+GiovanniArcudi@users.noreply.github.com>
2020-03-17 15:02:32 +0000

    Update JavaFileUtil.java

--- ExerciseSheet1/JavaFileUtil.java
+++ ExerciseSheet1/JavaFileUtil.java
@@ -1,12 +1,12 @@
 /** EXERCISE:
-   In this exercise you will write a Java application that concatenates several Java files into one file.
+    In this exercise you will write a Java application that concatenates several Java files into one file.
 
-   Create a class called JavaFileUtil that implements ConcatenateJavaFiles (package uk.ac.soton.ecs.comp1206.labtestlibrary.interfaces.io).
-   Implement the concatenateJavaFiles(String dirName, String fileName) method in the following way. Find all non-directory
-   files contained in directory dirName whose name ends in ".java" and concatenate them in to a single file called fileName
-   stored in dirName. Do not delete any files and please backup any of your own files before testing your solution.
+    Create a class called JavaFileUtil that implements ConcatenateJavaFiles (package uk.ac.soton.ecs.comp1206.labtestlibrary.interfaces.io).
+    Implement the concatenateJavaFiles(String dirName, String fileName) method in the following way. Find all non-directory
+    files contained in directory dirName whose name ends in ".java" and concatenate them in to a single file called fileName
+    stored in dirName. Do not delete any files and please backup any of your own files before testing your solution.
 
-   If the first given argument is not a directory, throw an IllegalArgumentException with a suitable error message.*/
+    If the first given argument is not a directory, throw an IllegalArgumentException with a suitable error message.*/
 
 import uk.ac.soton.ecs.comp1206.labtestlibrary.interfaces.io.ConcatenateJavaFiles;
 import java.io.*;
